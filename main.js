e7e54e4032d55be13040a01e9b3188535048f7b9
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-05-14 22:03:06 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const overlay = document.querySelector('.overlay');
 /* Looping through images */
 for (let i = 1; i <= 5; i++){
 const newImage = document.createElement('img');
-newImage.setAttribute('src',`pic${i}.jpg`);
+newImage.setAttribute('src',`images/pic${i}.jpg`);
 newImage.addEventListener('click', function() {
     displayedImage.setAttribute('src', newImage.getAttribute("src"))
 })
@@ -16,3 +16,12 @@ thumbBar.appendChild(newImage);
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function (e) {
+    if (btn.textContent == 'Darken'){
+       overlay.style.backgroundColor = 'rgb(0,0,0,0.5)';
+       e.target.textContent = 'lighten';
+    }else {
+        e.target.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0,0,0,0)';
+    }
+}
